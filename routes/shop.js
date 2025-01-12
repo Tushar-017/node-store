28b0ce83f26df2e383e1bb96e5d56fcb61e93960
Tushar Rajput <tusharo17rajput@gmail.com>
2025-01-12 21:46:44 +0530

    Enhance views and styles: Convert HTML files to EJS, add new form and product styles, and update routing for product management. Introduce includes for head, navigation, and end sections to streamline layout.

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,23 @@
 const path = require("path")
 const express = require("express")
 
-const rootDir = require("../utils/path")
+// const rootDir = require("../utils/path")
+// rootDir will only be used in the file if we have a path.js file in the utils folder
+const adminData = require("./admin")
 
 const router = express.Router()
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"))
+  const products = adminData.products
+  // console.log(adminData.products)
+  // res.sendFile(path.join(rootDir, "views", "shop.html"))
+  // res.sendFile(path.join(__dirname, "../", "views", "shop.html"))
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProduct: products.length > 0,
+  })
 })
 
 module.exports = router
