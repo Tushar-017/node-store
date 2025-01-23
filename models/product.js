b9477069f0c31ef096a1fef539ed8d2dd5f87a36
Tushar Rajput <tusharo17rajput@gmail.com>
2025-01-23 08:59:56 +0530

    modified the routes and updated the mvc

--- models/product.js
+++ models/product.js
@@ -1,15 +1,44 @@
-const products = []
+const fs = require("fs")
+const path = require("path")
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+)
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([])
+    } else {
+      try {
+        cb(JSON.parse(fileContent))
+      } catch (parseError) {
+        // If JSON parsing fails, return empty array
+        cb([])
+      }
+    }
+  })
+}
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t
+  constructor(title, imageUrl, price, description) {
+    this.title = title
+    this.imageUrl = imageUrl
+    this.description = description
+    this.price = price
   }
 
   save() {
-    products.push(this)
+    getProductsFromFile((products) => {
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err)
+      })
+    })
   }
 
-  static fetchAll() {
-    return products
+  static fetchAll(cb) {
+    getProductsFromFile(cb)
   }
 }
