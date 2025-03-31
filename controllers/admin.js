33a5190984158a0b1c513e428cf870595655021a
Tushar Rajput <tusharo17rajput@gmail.com>
2025-03-31 17:52:05 +0530

    Implement user and order management: Introduce Sequelize models for User, Order, and CartItem; refactor product handling in controllers; enhance cart and order functionalities; update views for cart and orders; and improve database synchronization.

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,12 +10,20 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, price, description } = req.body
-  const product = new Product(null, title, imageUrl, price, description)
-
-  product
-    .save()
-    .then(() => res.redirect("/"))
-    .catch((err) => console.log(err))
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
+    .then((result) => {
+      console.log("Created Product")
+      res.redirect("/admin/products")
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 exports.getEditProduct = (req, res, next) => {
@@ -24,48 +32,67 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/")
   }
   const prodId = req.params.productId
-  Product.findById(prodId, (product) => {
-    if (!product) {
-      return res.redirect("/")
-    }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: true,
-      product: product,
+  // Product.findByPk(prodId)
+  req.user
+    .getProducts({ where: { id: prodId } })
+    .then((products) => {
+      const product = products[0]
+      if (!product) {
+        return res.redirect("/")
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: true,
+        product: product,
+      })
     })
-  })
+    .catch((err) => console.log(err))
 }
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId
-  const updateTitle = req.body.title
-  const updatePrice = req.body.price
-  const updateImageUrl = req.body.imageUrl
-  const updateDesc = req.body.description
-  const updateProduct = new Product(
-    prodId,
-    updateTitle,
-    updateImageUrl,
-    updatePrice,
-    updateDesc
-  )
-  updateProduct.save()
-  res.redirect("/admin/products")
+  const updatedTitle = req.body.title
+  const updatedPrice = req.body.price
+  const updatedImageUrl = req.body.imageUrl
+  const updatedDesc = req.body.description
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle
+      product.price = updatedPrice
+      product.imageUrl = updatedImageUrl
+      product.description = updatedDesc
+      return product.save()
+    })
+    .then((result) => {
+      console.log("Updated Product!")
+      res.redirect("/admin/products")
+    })
+    .catch((err) => console.log(err))
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
+  req.user
+    .getProducts()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      })
     })
-  })
+    .catch((err) => console.log(err))
 }
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId
-  Product.deleteById(prodId)
-  res.redirect("/admin/products")
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy()
+    })
+    .then((result) => {
+      console.log("Destroy Product!!")
+      res.redirect("/admin/products")
+    })
+    .catch((err) => console.log(err))
 }
--- utils/database.js
+++ utils/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize")
 const sequelize = new Sequelize("node-complete", "root", "Tea1965@z", {
   dialect: "mysql",
   host: "localhost",
+  logging: console.log, // This will show SQL queries in console
 })
 
 module.exports = sequelize
