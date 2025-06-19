5a8cdc80d305e9e464b76bb867843e43a8ea4680
Tushar Rajput <tusharo17rajput@gmail.com>
2025-06-19 09:50:35 +0530

    product deletion with DOM manipulation

--- controllers/admin.js
+++ controllers/admin.js
@@ -170,23 +170,22 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
         return next(new Error("Product not found"));
       }
       fileHelper.deleteFile(product.imageUrl);
-      Product.deleteOne({ _id: prodId, userId: req.user._id });
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
     })
     .then(() => {
       console.log("DELETED PRODUCT!");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Success!" });
+      // res.redirect("/admin/products");
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.error(500).json({ message: "Deleting product failed." });
     });
 };
